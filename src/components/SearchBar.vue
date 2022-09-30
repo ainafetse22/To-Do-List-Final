5310930f531c09feec77b510a472f976d42b64c4
Estefania A <zestefania.amundaray@gmail.com>
2022-09-30 23:48:48 +0200

    search bar

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -6,7 +6,7 @@
          rounded-lg block p-2.5" id="search"
         v-model="searchTerm" placeholder="search a task...">
     </label>
-    <div class="absolute bg-orange-600 right-0 z-10 w-3/4 origin-top-right
+    <div class="absolute bg-orange-600 right-0 z-10 w-3/4 lg:w-1/2 origin-top-right
      rounded-md shadow-lg ring-1 ring-black ring-opacity-5
       focus:outline-none" role="menu" aria-orientation="vertical"
       aria-labelledby="menu-button" tabindex="-1">
